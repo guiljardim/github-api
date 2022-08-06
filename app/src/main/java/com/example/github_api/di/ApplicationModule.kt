1f37f9b2f7d0d055b6bdbee1e459311a3f07c6de
gjardim <guiljlima@gmail.com>
2022-08-06 07:07:27 -0300

    configurando retrofit

---
+++ app/src/main/java/com/example/github_api/di/ApplicationModule.kt
@@ -0,0 +1,43 @@
+package com.example.github_api.di
+
+import com.example.github_api.BuildConfig.BASE_URL
+import com.example.github_api.BuildConfig.DEBUG
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else OkHttpClient
+        .Builder()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build()
+}
